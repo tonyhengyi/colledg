468057a4fc96b98bb0581ed16fabadb7aac591bd
wq <1906173192@qq.com>
2023-10-12 19:29:15 +0800

    add design.h (uncomplete)

---
+++ design.h
@@ -0,0 +1,23 @@
+#ifndef ALL_USE
+#include<bits/stdc++.h>
+
+class repository{
+    void add_commodity(commdity *com);
+    void del_commodity();
+    
+};
+
+class commdity{
+};
+
+class original_factory{
+};
+
+class admin{
+};
+
+class consumer{
+};
+
+
+#endif
