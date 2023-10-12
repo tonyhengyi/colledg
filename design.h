f1623c225b7262e333ce6d94fd2163f7ee5c3353
wq <1906173192@qq.com>
2023-10-12 21:55:41 +0800

    more info
    add commdity_type(enum)
    
    add class order_list
    
    modify class consumer:
    - add have order_list
    - buy_commodity will add pair<commdity_type,cnt> to its order_list

--- design.h
+++ design.h
@@ -1,24 +1,51 @@
 #ifndef ALL_USE
 #include<bits/stdc++.h>
+using namespace std;
 
-class repository{
-    void add_commodity(commdity *com);
+enum class commodity_type {
+    TYPE_1,
+    TYPE_2,
+    TYPE_3
+};
+
+class repository {
+    void add_commodity();
     void del_commodity();
-    
+
 };
 
-class commdity{
+class admin {
+
 };
 
-class original_factory{
-};
+class order_list {
+    private:
+    vector<pair<commodity_type, int>>list;
 
-class admin{
+    public:
+    void add_to_list(pair<commodity_type, int> todo) {
+        list.push_back(todo);
+    }
+
+    vector<pair<commodity_type, int>> get_list() {
+        return list;
+    }
+    bool clean_list() {
+        list.clear();
+    }
 };
 
 class consumer {
-    
+    order_list buy_list;
+    void  buy_commodity(commodity_type ty, unsigned int cnt) {
+        buy_list.add_to_list(pair<commodity_type, unsigned int>{ty, cnt});
+    }
 };
+
+
+class commdity { };
+
+class original_factory { };
 
 
 #endif
