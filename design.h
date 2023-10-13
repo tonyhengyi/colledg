c1f2cbd3ac8e432660508a0cd68f8ca177ba0de3
wq <1906173192@qq.com>
2023-10-13 17:33:01 +0800

    大体完成

--- design.h
+++ design.h
@@ -6,39 +6,129 @@ enum class commodity_type {
     TYPE_1,
     TYPE_2,
     TYPE_3
+
 };
 
 class repository {
-    void add_commodity();
-    void del_commodity();
+    public:
+
+    static repository& get_instance() {
+        static repository repo;
+        return repo;
+    }
+
+    unsigned get_cnt_of_type(commodity_type ty) {
+        return list[ty];
+    }
+
+    void add(commodity_type ty, unsigned cnt) {
+        cout << "repo: +" << cnt << endl;
+        list[ty] += cnt;
+    }
+
+    void del(commodity_type ty, unsigned  cnt) {
+
+        if (list[ty] > cnt) {
+            list[ty] -= cnt;
+            cout << "repo: -" << cnt << "\n";
+
+        }
+        else {
+            assert(list[ty] >= cnt);
+            cout << "repo: exceed sub cnt,so don't redece\n";
+        }
+    }
+    //todo
+    void get_info() {
+        for (auto item : list) {
+            if (item.first == commodity_type::TYPE_1)
+                cout << "type1: " << item.second << '\n';
+        }
+    }
+    private:
+    unordered_map<commodity_type, unsigned > list;
+
+
+    repository() { };
+
+    repository(const repository& repo);
+
+    repository& operator = (const repository&) = delete;
+
 
 };
 
 class admin {
 
+    public:
+    void add_to_repo(unordered_map<commodity_type, unsigned>list, repository& repo) {
+        for (auto item : list) {
+
+            repo.add(item.first, item.second);
+        }
+    }
+
+    void del_to_repo(unordered_map<commodity_type, unsigned>list, repository& repo) {
+        for (auto item : list) {
+            //transaction character
+            if (item.second > repo.get_cnt_of_type(item.first)) {
+                cout << "exceed cnt,admin ALL del failed\n ";
+                return;
+            }
+        }
+        for (auto item : list) {
+
+            repo.del(item.first, item.second);
+        }
+    }
 };
 
 class order_list {
     private:
-    vector<pair<commodity_type, int>>list;
+    unordered_map<commodity_type, unsigned>list;
 
     public:
-    void add_to_list(pair<commodity_type, int> todo) {
-        list.push_back(todo);
+    void add_to_list(commodity_type ty, unsigned  cnt) {
+        cout << "order list: +" << cnt << endl;
+        list[ty] += cnt;
     }
 
-    vector<pair<commodity_type, int>> get_list() {
+    void reduce_to_list(commodity_type ty, unsigned cnt) {
+        if (list[ty] < cnt) {
+            cout << "exceed sub cnt,so DON't redece\n";
+        }
+        else {
+            list[ty] -= cnt;
+            cout << "order list: -" << cnt << "\tnow have: " << list[ty] << '\n';
+        }
+        return;
+    }
+
+    unordered_map<commodity_type, unsigned> get_list() {
         return list;
     }
-    bool clean_list() {
+
+    void clean_list() {
         list.clear();
     }
 };
 
 class consumer {
     order_list buy_list;
-    void  buy_commodity(commodity_type ty, unsigned int cnt) {
-        buy_list.add_to_list(pair<commodity_type, unsigned int>{ty, cnt});
+
+    public:
+    void  buy_commodity(commodity_type ty, unsigned  cnt) {
+        cout << "consumer: ";
+        buy_list.add_to_list(ty, cnt);
+    }
+
+    void reduce_commodity(commodity_type ty, unsigned  cnt) {
+        cout << "consumer: ";
+        buy_list.reduce_to_list(ty, cnt);
+    }
+
+    order_list get_final_order() {
+        return buy_list;
     }
 };
 
--- test.cpp
+++ test.cpp
@@ -1,9 +1,26 @@
-#include<iostream>
-using namespace std;
+#include"design.h"
 
-int main()
-{
-    
-    cout<<" 1"<<endl;
+void wq_test() {
+    repository& repo = repository::get_instance();
+    admin admin_one;
+    order_list test_list;
+
+    test_list.add_to_list(commodity_type::TYPE_1, 10);
+    test_list.add_to_list(commodity_type::TYPE_2, 10);
+    test_list.add_to_list(commodity_type::TYPE_3, 10);
+
+    admin_one.add_to_repo(test_list.get_list(), repo);
+
+
+    consumer con;
+    con.buy_commodity(commodity_type::TYPE_1, 5);
+    con.reduce_commodity(commodity_type::TYPE_1, 3);
+
+    admin_one.del_to_repo(con.get_final_order().get_list(), repo);
+
+    repo.get_info();
+}
+int main() {
+    wq_test();
     return 0;
 }
